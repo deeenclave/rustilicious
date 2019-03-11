bf22523f28a4f2073f07a08c68925ae231975b8d
rangarajl <rangaraj.l@gmail.com>
2019-03-10 22:20:46 -0400

    power of rust

--- main.rs
+++ main.rs
@@ -1,5 +1,5 @@
-mod print;
+mod types;
 
 fn main() {
-    print::printer();
+    types::run();
 }
---
+++ types.rs
@@ -0,0 +1,8 @@
+pub fn run() {
+    let mut decision: bool = 10 > 5;
+    decision = false;
+    let final_decision: bool = decision;
+    println!("Have you decided to rust yet -? {:?}", decision);
+}
+
+
